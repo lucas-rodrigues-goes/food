ae8f4c74abc1f8546f09ec3f273047de15e5a301
Lucas Góes <lucasrodriguesgoes@outlook.com>
2025-03-26 09:29:46 -0300

    Create: Structure Components
    Components based on the react native element View, used to structure the page.

---
+++ components/structure.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+const { View, Text } = require('react-native');
+
+// View component that attempts to center its content
+function Center({ children, style, ...rest }) {
+    const centerStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+        textAlign: 'center',
+    };
+
+    // Output
+    return (
+        <View style={{ ...centerStyle, ...style }} {...rest}>
+            {children}
+        </View>
+    );
+}
+
+// View component that aims to create a visual separation of an area, that is usually given a title
+function Container({ children, style, title, ...rest }) {
+    // Container default styling
+    const containerStyle = {
+        marginVertical: 10,
+        padding: 15,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        width: '100%',
+    };
+
+    // Optional title child
+    const titleStyle = {
+        fontSize: 16,
+        fontWeight: '600',
+        marginBottom: 10,
+        color: '#444',
+    }
+    const titleElement = title && <Text style={titleStyle}>{title}</Text> 
+
+    // Output
+    return (
+        <View style={{ ...containerStyle, ...style }} {...rest}>
+            {titleElement}
+            {children}
+        </View>
+    );
+}
+
+
+module.exports = { Center, Container };
