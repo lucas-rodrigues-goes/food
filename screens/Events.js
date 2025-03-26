ffd853e471007e09931e6586cf0b8244a2d333d6
Lucas Góes <lucasrodriguesgoes@outlook.com>
2025-03-26 19:19:12 -0300

    Update: Events
    Video now working

--- screens/Events.js
+++ screens/Events.js
@@ -1,15 +1,13 @@
 import * as React from 'react';
 
 // Components
-const { Text, Image, ScrollView, Linking } = require('react-native');
+const { Text, Image, ScrollView, Linking, Platform } = require('react-native');
 const { Video } = require('react-native-video');
 const { Center, Container } = require('../components/structure');
 const { Button } = require('../components/buttons')
 const styles = require('../style/general');
 
 // Assets
-const contactInfo = require('../assets/data/food_hall_contact_info.json')
-const logoImage = require('../assets/images/logo_food_hall.png')
 const artist1Video = require('../assets/videos/artist_1_video.mp4')
 
 
@@ -17,81 +15,21 @@ const artist1Video = require('../assets/videos/artist_1_video.mp4')
 module.exports = function Events({ navigation }) {
 
 	// Button Handlers
-	const handleOpenMap = () => {
-		const url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(contactInfo.address)}`
-		Linking.openURL(url)
-	}
-	const handleCall = () => Linking.openURL(`tel:${contactInfo.phone}`)
-	const handleWhatsApp = () => Linking.openURL(`https://wa.me/${contactInfo.whatsapp}`)
-	const handleEmail = () => Linking.openURL(`mailto:${contactInfo.email}?subject=Contato%20Pátio`)
 
 	return (
 		<ScrollView>
 		  <Center>
-			{/* Logo */}
-			<Image
-			  style={styles.logo}
-			  source={logoImage} 
-			/>
-	
+
 			{/* Eventos */}
 			<Container title={"Artista 1"}>
 				<Text style={styles.contactText}>Dia 26/03 as 20h</Text>
 				<Center>
-                    <Video  
-                        source={artist1Video}
-                        paused={false}
-                        style={styles.video}
-                        repeat={false}
-                        onError={(e) => console.log(e)}
-                    />
+				{Platform.OS === 'web' 
+					? (<video src={artist1Video} controls style={styles.video} />) 
+					: (<Video source={artist1Video} paused={false} style={styles.video} repeat={false} onError={(e) => console.log(e)} /> )}
 				</Center>
 			</Container>
-	
-			{/* Redes Sociais */}
-			<Container title={"Redes Sociais"}>
-				<Button
-					title="Facebook"
-					onPress={() => Linking.openURL(contactInfo.facebook)}
-					color="#3b5998"
-				/>
-				<Button
-					title="Instagram"
-					onPress={() => Linking.openURL(contactInfo.instagram)}
-					color="#E1306C"
-				/>
-			</Container>
-	
-			{/* Contato */}
-			<Container title={"Informações de contato"}>
-				<Text style={[styles.contactText, {marginTop: 15}]}>{contactInfo.phone}</Text>
-				<Button
-					title="Ligar Agora"
-					onPress={handleCall}
-					color="#007bff"
-				/>
-		
-				<Text style={[styles.contactText, {marginTop: 15}]}>WhatsApp</Text>
-				<Button
-					title="Enviar Mensagem"
-					onPress={handleWhatsApp}
-					color="#25D366"
-				/>
 
-				<Text style={[styles.contactText, {marginTop: 15}]}>Site Oficial</Text>
-				<Button
-					title="Visitar Website"
-					onPress={() => Linking.openURL(contactInfo.website)}
-					color="#6c757d"
-				/>
-
-				<Text style={[styles.contactText, {marginTop: 15}]}>E-mail</Text>
-				<Button
-					title="Enviar E-mail"
-					onPress={handleEmail}
-					color="#dc3545"
-				/>
-			</Container>
 		  </Center>
 		</ScrollView>
 	  );
