5f75a044ced666e808da07df9058b973d578ad42
Lucas Góes <lucasrodriguesgoes@outlook.com>
2025-03-26 10:08:12 -0300

    Update: App.js
    Updated variable names for better clarity.

--- App.js
+++ App.js
@@ -5,15 +5,15 @@ const Home = require('./screens/Home');
 
 // Components
 const { NavigationContainer } = require('@react-navigation/native')
-const Stack = require('@react-navigation/native-stack').createNativeStackNavigator()
+const { Navigator, Screen } = require('@react-navigation/native-stack').createNativeStackNavigator()
 
 // Output
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Inicio">
-        <Stack.Screen name="Inicio" component={Home} />
-      </Stack.Navigator>
+	<NavigationContainer>
+    	<Navigator initialRouteName="Inicio">
+    		<Screen name="Inicio" component={Home}/>
+    	</Navigator>
     </NavigationContainer>
   );
 }
