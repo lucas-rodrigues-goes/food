6b73f7424419be11ccdcb9b4839828ccb3631819
Lucas Góes <lucasrodriguesgoes@outlook.com>
2025-03-31 11:39:51 -0300

    Update: Events
    Changed properties, updated visual, media will now be pulled from web, instead of attempting to manage them locally through static imports.
    
    Also fixed missing key bug for the event list.

--- screens/Events.js
+++ screens/Events.js
@@ -13,24 +13,28 @@ const events_info = require('../assets/data/events_info.json')
 // Output
 module.exports = function Events({ navigation }) {
 	// Criar lista de eventos
-	const eventElements = []
+	const eventElements = [];
 	for (const evento of events_info) {
-		// Video source
-		const video = require("../assets/videos/artist_1_video.mp4")
-
-		// Add element to list
 		eventElements.push(
-			<Container title={evento.name}>
-				<Text style={styles.contactText}>{evento.date}</Text>
+			<Container key={evento.name} title={evento.name + " - " + evento.date}>
 				<Text style={styles.contactText}>{evento.description}</Text>
 				<Center>
-				{Platform.OS === 'web'
-					? (<video src={video} controls style={styles.video} />) 
-					: (<Video source={video} paused={false} style={styles.video} repeat={false} onError={(e) => console.log(e)} /> )}
+					{Platform.OS === 'web' ? (
+						<video src={evento.video} controls style={styles.video} />
+					) : (
+						<Video 
+							source={evento.video} 
+							paused={false} 
+							style={styles.video} 
+							repeat={false} 
+							onError={(e) => console.log(e)} 
+						/>
+					)}
 				</Center>
 			</Container>
-		)
+		);
 	}
+
 
 	return (
 		<ScrollView>
